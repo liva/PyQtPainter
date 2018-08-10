d8c53fe0539f9a559650896612783e2a7968e540
liva <sap.pcmail@gmail.com>
2018-08-10 15:53:46 +0900

    update to specify ROOT_DIR

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 # sample
+DW_ROOT_DIR=$(CURDIR)
 DW_CHECK_CODE:=[ ! -e /cpputest/lib/libCppUTest.a ]
 DW_CONTAINER:=livadk/cpputest:aac118d572d3c41bc9e3bed32c7ae8c19249784c
 DW_CONTAINER_NAME:=docker_wrapper_sample
--- rules.mk
+++ rules.mk
@@ -4,25 +4,25 @@ $$(error Please define the variable '$1')
 endif
 endef
 
+$(eval $(call dw_check_variables,DW_ROOT_DIR))
 $(eval $(call dw_check_variables,DW_CHECK_CODE))
 $(eval $(call dw_check_variables,DW_CONTAINER))
 $(eval $(call dw_check_variables,DW_CONTAINER_NAME))
 
-ROOT_DIR:=$(dir $(abspath $(lastword $(MAKEFILE_LIST))))
-RELATIVE_DIR:=$(shell bash -c "root_dir=$(abspath $(ROOT_DIR)); pwd=$(CURDIR); echo \$${pwd\#\$${root_dir}};")
+DW_RELATIVE_DIR:=$(shell bash -c "root_dir=$(abspath $(DW_ROOT_DIR)); pwd=$(CURDIR); echo \$${pwd\#\$${root_dir}};")
 
 HOST=$(shell if $(DW_CHECK_CODE); then echo "false"; fi)
 
 ifneq ($(HOST),)
 # host environment
-HOST_DIR:=$(abspath $(ROOT_DIR))
-SHARE_DIR:=/share
+DW_HOST_DIR:=$(abspath $(DW_ROOT_DIR))
+DW_SHARE_DIR:=/share
 
 define docker_wrapper
 	@docker rm $1 -f > /dev/null 2>&1 || :
-	docker run -d $(if $(CI),,-v $(HOST_DIR):$(SHARE_DIR)) -it --name $1 $2
-	$(if $(CI),docker cp $(HOST_DIR) $1:$(SHARE_DIR))
-	docker exec $1 sh -c "cd /share$(RELATIVE_DIR) && $3"
+	docker run -d $(if $(CI),,-v $(DW_HOST_DIR):$(DW_SHARE_DIR)) -it --name $1 $2
+	$(if $(CI),docker cp $(DW_HOST_DIR) $1:$(DW_SHARE_DIR))
+	docker exec $1 sh -c "cd /share$(DW_RELATIVE_DIR) && $3"
 	@echo ""
 	docker rm -f $1
 endef
@@ -35,7 +35,7 @@ attach_docker:
 	docker exec -it $(DW_CONTAINER_NAME) /bin/bash
 
 run_docker:
-	docker run --rm -v $(HOST_DIR):$(SHARE_DIR) -it $(DW_CONTAINER) /bin/bash
+	docker run --rm -v $(HOST_DIR):$(DW_SHARE_DIR) -it $(DW_CONTAINER) /bin/bash
 
 %:
 	$(call docker_wrapper,$(DW_CONTAINER_NAME),$(DW_CONTAINER),make $@)
